a87db085d3359de9e651fdf8c4a3198d28caccc7
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2024-03-05 01:55:33 +0530

    Remove the inline styles

--- ConnectAppMobile/App.tsx
+++ ConnectAppMobile/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import { UploadPostScreen } from './src/screens/UploadPostScreen';
+import { styles } from './src/styles/UploadPostScreen.styles';
 
 
 export default function App() {
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView
+      style={styles.SafeAreaView}
+    >
       <UploadPostScreen />
     </SafeAreaView>
   );
