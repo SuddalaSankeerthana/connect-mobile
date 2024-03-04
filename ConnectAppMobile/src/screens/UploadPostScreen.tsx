1d80cffec5e4a3bbc69beafffaa4678e3660ff10
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2024-03-05 01:00:34 +0530

    Add text upload with the styles

--- ConnectAppMobile/src/screens/UploadPostScreen.tsx
+++ ConnectAppMobile/src/screens/UploadPostScreen.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Text, View } from "react-native";
+import { styles } from "../styles/UploadPostScreen.styles";
 
 
 
 export const UploadPostScreen = () => {
- return (
-   
- );  
+    return (
+        <View style={styles.container}>
+            <Text style={styles.uploadText}>Upload</Text>
+        </View>
+    );
 }
