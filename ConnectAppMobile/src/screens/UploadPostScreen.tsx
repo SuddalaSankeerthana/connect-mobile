b58d3adb11aa5c8e77c2f920d6b21b0bcb2b46ca
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2024-03-05 01:45:29 +0530

    Wrap the icon and text in a blue box

--- ConnectAppMobile/src/screens/UploadPostScreen.tsx
+++ ConnectAppMobile/src/screens/UploadPostScreen.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import {Image, Text, View} from 'react-native';
-import {styles} from '../styles/UploadPostScreen.styles';
+import React from "react";
+import { Image, Text, View } from 'react-native';
+import { styles } from "../styles/UploadPostScreen.styles";
+
 
 export const UploadPostScreen = () => {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.uploadText}>Upload</Text>
-
-      <Image
-        style={styles.crossIcon}
-        source={require('../assets/cross-icon.png')}
-      />
-      <Image
-        style={styles.uploadIcon}
-        source={require('../assets/upload-icon.png')}
-      />
-      <Text style={styles.uploadImageText}>Upload the images</Text>
-      <Text style={styles.supportedFormatsText}>
-        Supported formats: JPEG, PNG, JPG
-      </Text>
-    </View>
-  );
+    return (
+      <>
+        <View style={styles.container}>
+          <Text style={styles.uploadText}>Upload</Text>
+          <Image
+            style={styles.crossIcon}
+            source={require('../assets/cross-icon.png')}
+          />
+          <View style={styles.blueBox}>
+            <Image
+              style={styles.uploadIcon}
+              source={require('../assets/upload-icon.png')}
+            />
+            <Text style={styles.uploadImageText}>Upload the images</Text>
+            <Text style={styles.supportedFormatsText}>
+              Supported formats: JPEG, PNG, JPG
+            </Text>
+          </View>
+        </View>
+      </>
+    );
 };
