3a387d3a5d581c376f3effb71ea28ef66d0314e1
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-05 11:45:04 +0530

    Create profile details and render in post details.

---
+++ ConnectAppMobile/src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import {Image, View, Text} from 'react-native';
+import {styles} from '../styles/HomeScreenBodyStyles';
+
+export function Profile({
+  profile_pic,
+  name,
+}: {
+  profile_pic: string;
+  name: string;
+}) {
+  return (
+    <>
+      <View style={styles.profileContainer}>
+        <Image source={{uri: profile_pic}} style={styles.profileImage} />
+        <Text>{name}</Text>
+      </View>
+    </>
+  );
+}
