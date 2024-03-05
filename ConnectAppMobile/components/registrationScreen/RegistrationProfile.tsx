a5ff7fd75bb0bb011722616bd340aa13bc03989b
sankeerthana <sankeerthana@Admins-MacBook-Pro-2.local>
2024-03-05 11:08:17 +0530

    Update Registraion profil component with edit icon

--- ConnectAppMobile/components/registrationScreen/RegistrationProfile.tsx
+++ ConnectAppMobile/components/registrationScreen/RegistrationProfile.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, TouchableOpacity} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 const RegistraionProfile = () => {
   return (
-    // <SafeAreaView>
-    <View style={{}}>
+    <View style={{display: 'flex'}}>
       <Image
-        style={{width: 80, height: 80}}
+        style={{width: 80, height: 80, resizeMode: 'cover'}}
         source={require('../../images/png/user.png')}></Image>
+      <TouchableOpacity>
+        <Image
+          style={{width: 20, height: 20, top: -15, marginStart: 'auto'}}
+          source={require('../../images/png/Group.png')}></Image>
+      </TouchableOpacity>
     </View>
-    // {/* </SafeAreaView> */}
   );
 };
 export default RegistraionProfile;
