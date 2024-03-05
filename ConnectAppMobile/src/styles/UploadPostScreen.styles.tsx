08769762d49545d312b59bc08b50817a9264dab2
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2024-03-05 10:44:15 +0530

    Update the styles

--- ConnectAppMobile/src/styles/UploadPostScreen.styles.tsx
+++ ConnectAppMobile/src/styles/UploadPostScreen.styles.tsx
@@ -1,6 +1,5 @@
 import {StyleSheet} from 'react-native';
 
-
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,7 +19,6 @@ export const styles = StyleSheet.create({
   },
   uploadIcon: {
     alignItems: 'center',
-    marginTop: 300,
     marginVertical: 5,
   },
   supportedFormatsText: {
@@ -30,25 +28,50 @@ export const styles = StyleSheet.create({
     marginVertical: 5,
   },
   blueBox: {
-    backgroundColor: 'rgba(238, 246, 255, 1)',
-    alignItems: 'center',
-    borderColor: 'blue',
+    height: 120,
     width: 300,
-    height: 200,
-    padding: 20,
+    display: 'flex',
+    borderRadius: 5,
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderColor: '#B2CFF1',
+    backgroundColor: '#EEF6FF',
+    marginTop:290,
+    justifyContent: 'center',
+    alignItems:'center'
+
   },
   SafeAreaView: {
     flex: 1,
   },
   imageContainer: {
+    alignItems: 'center',
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'center',
+    marginBottom: 350,
   },
   selectedImage: {
-    width: 100,
-    height: 100,
+    width: 150,
+    height: 150,
     margin: 5,
     resizeMode: 'cover',
+  },
+  textInputContainer: {
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
+    alignItems: 'center',
+    marginRight: 70,
+    marginLeft: 20,
+    width: '90%',
+    height: 50,
+    bottom: 150,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.3,
+    shadowRadius: 2,
   },
 });
