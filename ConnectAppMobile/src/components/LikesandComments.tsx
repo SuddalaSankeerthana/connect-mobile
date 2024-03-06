514f10a09464dfacff728cf64bc4fcc2611f9dbc
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-06 09:47:24 +0530

    render like and comment icons and like count.

---
+++ ConnectAppMobile/src/components/LikesandComments.tsx
@@ -0,0 +1,23 @@
+import {Image, Text, View} from 'react-native';
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
+import { styles } from '../styles/HomeScreenBodyStyles';
+
+
+export function LikesAndComments({likes_count}: {likes_count: number}) {
+  return (
+   
+      <View style={styles.likesAndComment}>
+        <View style={styles.heartIcon}>
+      <FontAwesomeIcon icon={farHeart} size={27}/>
+      </View>
+      <View>
+      <Text>{likes_count}</Text>
+      </View>
+      <View>
+      <Image source={require('../assets/commentIcon.png')} style={styles.commentIcon}></Image>
+      </View>
+      </View>
+  );
+}
