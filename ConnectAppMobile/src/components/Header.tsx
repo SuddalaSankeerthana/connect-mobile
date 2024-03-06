66518ee8cb7e2756838b5464dda1563a03c214e0
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-06 12:25:08 +0530

    add menu bar in the header and show pop up on clicking menu bar, on clicking log out in pop up navigates to upload screen.

--- ConnectAppMobile/src/components/Header.tsx
+++ ConnectAppMobile/src/components/Header.tsx
@@ -1,12 +1,18 @@
-import {FlatList, View, Text, Image} from 'react-native';
+import {Button, View, Text, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {styles} from '../styles/HomeScreenBodyStyles';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
 import {faPlusSquare} from '@fortawesome/free-regular-svg-icons';
 import {Profile} from './Profile';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
+import {useState} from 'react';
+import {Menu, MenuItem, MenuDivider} from 'react-native-material-menu';
 
-export function Header() {
+export function Header({navigation}: any) {
+  const [visible, setVisible] = useState(false);
+  const hideMenu = () => setVisible(false);
+
+  const showMenu = () => setVisible(true);
   return (
     <View style={styles.header}>
       <View style={styles.logoAndTitleContainer}>
@@ -15,16 +21,35 @@ export function Header() {
       </View>
       <View style={styles.iconsContainer}>
         <View>
-        <Profile
-          profile_pic="https://i.pinimg.com/originals/d8/2b/ca/d82bca57dcaa9ceb4c827e3d6ab9fcfe.jpg"
-          name="Lavanya"
-          containerStyle={styles.headerProfileContainer}
-          imageStyle={styles.headerProfile}
-          profileName={styles.profileName}
-        />
+          <Profile
+            profile_pic="https://i.pinimg.com/originals/d8/2b/ca/d82bca57dcaa9ceb4c827e3d6ab9fcfe.jpg"
+            name="Lavanya"
+            containerStyle={styles.headerProfileContainer}
+            imageStyle={styles.headerProfile}
+            profileName={styles.profileName}
+          />
         </View>
-        <FontAwesomeIcon icon={faPlusSquare} size={27} style={styles.addIcon} />
-        <FontAwesomeIcon icon={faBars} size={27} style={styles.menuBar} />
+        <TouchableOpacity onPress={() => navigation.navigate('Upload')}>
+          <FontAwesomeIcon
+            icon={faPlusSquare}
+            size={27}
+            style={styles.addIcon}
+          />
+        </TouchableOpacity>
+        <Menu
+          visible={visible}
+          anchor={
+            <TouchableOpacity onPress={showMenu}>
+              <FontAwesomeIcon icon={faBars} size={27} style={styles.menuBar} />
+            </TouchableOpacity>
+          }
+          onRequestClose={hideMenu}>
+          <MenuItem onPress={hideMenu} style={styles.logOutContainer}>
+            <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+              <Text>Log out</Text>
+            </TouchableOpacity>
+          </MenuItem>
+        </Menu>
       </View>
     </View>
   );
