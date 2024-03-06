cdabc8659ca2272ef3e7c38780e78227ae3c158a
PagidipallyDivyasai <divya.pagidipally@everest.engineering>
2024-03-07 01:07:13 +0530

    Add input validations in login screen

--- ConnectAppMobile/screens/Login.tsx
+++ ConnectAppMobile/screens/Login.tsx
@@ -5,28 +5,31 @@ import InputField from '../src/components/InputFields';
 import LoginButton from '../src/components/Button';
 import Caption from '../src/components/Caption';
 import {styles} from '../src/styles/Login.styles';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useNavigation } from '@react-navigation/native';
 
-function LoginScreen(navigation: any): React.JSX.Element {
-  const [email, setEmal]= React.useState('');
+function LoginScreen(): React.JSX.Element {
+
+  const [email, setEmail]= React.useState('');
   const [password, setPassword]= React.useState('');
   return (
     <SafeAreaView>
       <View style={styles.mainContainer}>
         <View style={styles.loginMainContainer}>
           <LoginLogo />
-          <InputField onChangeText={setEmal} value={email} label="Email"  source={require('../src/assets/email.png')}
+          <InputField onChangeText={setEmail} value={email.toLowerCase()} label="Email"  source={require('../src/assets/email.png')}
             style={styles.emailIcon}/>
           <InputField
             onChangeText={setPassword}
-            value={password}
+            value={password.toLowerCase()}
             label="Password"
             source={require('../src/assets/password.png')}
             style={styles.passwordIcon}
           />
-          <LoginButton />
+          <LoginButton password={password} email={email} />
           <View style={styles.noAccount}>
           <Caption text="Don't have an account?" />
-          <Caption text="Sign Up" style={styles.signUp} onPress={()=>{}}/>
+          <Caption text="Sign Up" style={styles.signUp} />
           </View>
         </View>
       </View>
