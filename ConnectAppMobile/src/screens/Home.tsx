3a83b1cd153a86dc4b45f92aebe082f1848915fb
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-06 09:54:35 +0530

    remove unused code and format file.

--- ConnectAppMobile/src/screens/Home.tsx
+++ ConnectAppMobile/src/screens/Home.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
 import {Button, View, Text} from 'react-native';
-import { Body } from '../components/Body';
+import {SafeAreaView} from 'react-native';
+import {Body} from '../components/Body';
 
 const HomeScreen = ({navigation}: any) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        alignContent: 'center',
-        justifyContent: 'center',
-      }}>
-        <Body/>
-      {/* <Text>Home screen</Text> */}
-      {/* <Button
-        title="Upload"
-        onPress={() => navigation.navigate('Upload')}></Button>
-      <Button
-        title="Log-out"
-        onPress={() => navigation.navigate('Login')}></Button> */}
-    </View>
+    <SafeAreaView>
+      <View>
+        <Body />
+      </View>
+    </SafeAreaView>
   );
 };
 export default HomeScreen;
