f11dbce6f32379eb8fe30d6c919a819c233c44cd
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-06 11:15:08 +0530

    add props to reuse the component.

--- ConnectAppMobile/src/components/Profile.tsx
+++ ConnectAppMobile/src/components/Profile.tsx
@@ -5,15 +5,21 @@ import {styles} from '../styles/HomeScreenBodyStyles';
 export function Profile({
   profile_pic,
   name,
+  containerStyle,
+  imageStyle,
+profileName
 }: {
   profile_pic: string;
   name: string;
+  containerStyle?:any,
+  imageStyle:any
+  profileName?:any
 }) {
   return (
     <>
-      <View style={styles.profileContainer}>
-        <Image source={{uri: profile_pic}} style={styles.profileImage} />
-        <Text>{name}</Text>
+      <View style={containerStyle}>
+        <Image source={{uri: profile_pic}} style={imageStyle} />
+        <Text style={profileName}>{name}</Text>
       </View>
     </>
   );
