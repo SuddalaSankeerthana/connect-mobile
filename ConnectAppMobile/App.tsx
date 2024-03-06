d0b39dc133119d4c34424ac6db5d0f22579919d9
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2024-03-06 10:21:49 +0530

    Implement navigation for the screens

--- ConnectAppMobile/App.tsx
+++ ConnectAppMobile/App.tsx
@@ -1,15 +1,46 @@
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import { UploadPostScreen } from './src/screens/UploadPostScreen';
-import { styles } from './src/styles/UploadPostScreen.styles';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/Home';
+import LoginScreen from './src/screens/Login';
+import RegistrationScreen from './src/screens/Registration';
+import UploadScreen from './src/screens/Upload';
 
+type AppStackParamList = {
+  Home: undefined;
+  Upload: undefined;
+  Login: undefined;
+  Registration: undefined;
+};
 
-export default function App() {
+const Stack = createNativeStackNavigator<AppStackParamList>();
+
+function AppStack() {
   return (
-    <SafeAreaView
-      style={styles.SafeAreaView}
-    >
-      <UploadPostScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Registration"
+          component={RegistrationScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Upload"
+          component={UploadScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+export default AppStack;
