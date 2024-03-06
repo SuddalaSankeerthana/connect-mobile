126d08fd85d174a72dc61be7dd2eeb56106317fe
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2024-03-06 13:17:56 +0530

    Update the test

--- ConnectAppMobile/__tests__/App.test.tsx
+++ ConnectAppMobile/__tests__/App.test.tsx
@@ -4,21 +4,23 @@
 
 import 'react-native';
 import React from 'react';
+import {render} from '@testing-library/react-native';
+
+
 import App from '../App';
-
-// Note: import explicitly to use the types shipped with jest.
-import {it} from '@jest/globals';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-
+import UploadScreen from '../src/screens/Upload';
 jest.mock('react-native-image-crop-picker', () => {
   return {
     openPicker: jest.fn().mockImplementation(() => Promise.resolve()),
   };
 });
+jest.mock('@react-navigation/native-stack');
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('UploadPostScreen component', () => {
+  test('renders correctly without selected images', () => {
+    const { getByText, queryByTestId } = render(<UploadScreen />);
+    expect(getByText('Upload')).toBeTruthy();
+    expect(queryByTestId('cancel-button')).toBeNull();
+    expect(queryByTestId('upload-button')).toBeNull();
+  });
 });
