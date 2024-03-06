20b890fe992f412cf56b806fb9c5597bcf1621fb
lavanyadasari <lavanya.dasari@everest.engineering>
2024-03-06 10:51:08 +0530

    Add comment component

--- ConnectAppMobile/src/components/LikesandComments.tsx
+++ ConnectAppMobile/src/components/LikesandComments.tsx
@@ -1,23 +1,22 @@
 import {Image, Text, View} from 'react-native';
 import React from 'react';
 import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
-import { faHeart as farHeart } from '@fortawesome/free-regular-svg-icons'
-import { styles } from '../styles/HomeScreenBodyStyles';
-
+import {faHeart as farHeart} from '@fortawesome/free-regular-svg-icons';
+import {styles} from '../styles/HomeScreenBodyStyles';
+import {Comment} from './comment/Comment';
 
 export function LikesAndComments({likes_count}: {likes_count: number}) {
   return (
-   
-      <View style={styles.likesAndComment}>
-        <View style={styles.heartIcon}>
-      <FontAwesomeIcon icon={farHeart} size={27}/>
+    <View style={styles.likesAndComment}>
+      <View style={styles.heartIcon}>
+        <FontAwesomeIcon icon={farHeart} size={27} />
       </View>
       <View>
-      <Text>{likes_count}</Text>
+        <Text>{likes_count}</Text>
       </View>
       <View>
-      <Image source={require('../assets/commentIcon.png')} style={styles.commentIcon}></Image>
+        <Comment />
       </View>
-      </View>
+    </View>
   );
 }
