7e28497e065160caa9116f0f35d3ba5534807dfb
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-06 09:50:14 +0530

    add caption and likes and comment component in post item.

--- ConnectAppMobile/src/components/PostItem.tsx
+++ ConnectAppMobile/src/components/PostItem.tsx
@@ -1,11 +1,14 @@
 import {images, PostDetails} from '../seeder/PostDetails';
-import Carousel, { Pagination } from "react-native-snap-carousel";
-import {View, Text, Image} from 'react-native';
+import Carousel, {Pagination} from 'react-native-snap-carousel';
+import {View, ScrollView} from 'react-native';
 import {PostImage} from './PostImage';
 import {styles} from '../styles/HomeScreenBodyStyles';
-import { Profile } from './Profile';
-import { useState, useRef } from 'react';
+import {Profile} from './Profile';
+import {useState, useRef} from 'react';
 import React from 'react';
+import {LikesAndComments} from './LikesandComments';
+import { Text } from 'react-native-svg';
+import { Caption } from './Caption';
 
 const renderImages = ({item}: {item: images}) => {
   return (
@@ -19,40 +22,47 @@ export function PostItem({
   profile_pic,
   name,
   images,
-  description,
   likes_count,
+  description
 }: PostDetails) {
-    const [page, setPage] = useState(0);
-   const isCarousel = useRef(null);
+  const [page, setPage] = useState(0);
+  const isCarousel = useRef(null);
   return (
-   <View style={styles.postConitainer}>
-<Profile profile_pic={profile_pic} name={name}/>
+    <View style={styles.postConitainer}>
+      <Profile profile_pic={profile_pic} name={name} />
       <Carousel
-      ref={isCarousel}
-      onSnapToItem={(page) => setPage(page)}
+        ref={isCarousel}
+        onSnapToItem={page => setPage(page)}
         layout={'default'}
         data={images}
         renderItem={renderImages}
+        sliderHeight={100}
         sliderWidth={400}
         itemWidth={350}
       />
       <Pagination
-            activeDotIndex={page}
-            carouselRef={isCarousel}
-            tappableDots={true}
-            inactiveDotOpacity={0.4}
-            inactiveDotScale={0.6}
-            dotsLength={images.length}
-            dotStyle={{
-               width: 10,
-               borderRadius: 18,
-               backgroundColor: "#0074FF",
-            }}
-            inactiveDotStyle={{
-               backgroundColor: "grey",
-            }}
-         />
-      </View>
-    
+        activeDotIndex={page}
+        carouselRef={isCarousel}
+        tappableDots={true}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+        dotsLength={images.length}
+        dotStyle={{
+          width: 10,
+          borderRadius: 18,
+          backgroundColor: '#0074FF',
+        }}
+        containerStyle={{
+          paddingTop: -20,
+        }}
+        inactiveDotStyle={{
+          backgroundColor: 'grey',
+        }}
+      />
+
+      <LikesAndComments likes_count={likes_count} />
+
+      <Caption description={description} />
+    </View>
   );
 }
