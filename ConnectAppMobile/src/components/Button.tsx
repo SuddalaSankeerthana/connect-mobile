4d5368b7477147234a765bb2ad59eb0a0e442cf7
PagidipallyDivyasai <divya.pagidipally@everest.engineering>
2024-03-06 15:40:26 +0530

    Modify button component from button to text

--- ConnectAppMobile/src/components/Button.tsx
+++ ConnectAppMobile/src/components/Button.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import {SafeAreaView, Text, View, Image, Button} from 'react-native';
+import {SafeAreaView, Text, View, Image, Button, Pressable, TouchableOpacity} from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
+import { styles } from '../styles/Login.styles';
 
 function LoginButton(): React.JSX.Element {
   return (
-    <View >
-      <Button  onPress={() => {}} title="Login"/>
-    </View>
+    <TouchableOpacity onPress={()=>{}} >
+        <Text style={styles.loginButton}>Login</Text>
+      </TouchableOpacity>
   );
 }
 
