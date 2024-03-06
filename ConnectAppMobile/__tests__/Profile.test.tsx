2d99ec05ba4f955fd22e15ba8eca0ee05704c682
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-06 17:01:30 +0530

    add test for rendering profile on screen.

---
+++ ConnectAppMobile/__tests__/Profile.test.tsx
@@ -0,0 +1,16 @@
+import 'react-native';
+import React from 'react';
+import {render, screen} from '@testing-library/react-native';
+import {Profile} from '../src/components/Profile';
+
+test('renders correctly', () => {
+  render(
+    <Profile
+      profile_pic="https://static.vecteezy.com/system/resources/thumbnails/006/541/188/small/bearded-male-cartoon-character-with-sunglasses-minimalist-cartoon-avatar-profile-illustration-free-vector.jpg"
+      name="Lavanya"
+    />,
+  );
+
+  expect(screen.getByTestId('profile')).toBeDefined();
+  expect(screen.getByText('Lavanya')).toBeDefined();
+});
