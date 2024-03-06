03f654afc7c7f3aa3acddf31751613d545a9e92d
PagidipallyDivyasai <divya.pagidipally@everest.engineering>
2024-03-06 15:43:00 +0530

    Add styles for the login screen

--- ConnectAppMobile/src/components/Logo.tsx
+++ ConnectAppMobile/src/components/Logo.tsx
@@ -6,23 +6,23 @@ import Caption from './Caption';
 function Logo(): React.JSX.Element {
   return (
     <>
-      <View >
+      <View style={styles.container}>
         <View style={styles.logoTitle}>
-          <View testID="logo" >
-            <Image source={require('../assets/logo2.png')} />
+          <View testID="logo">
+            <Image source={require('../assets/logo2.png')} style={styles.logo}/>
           </View>
           <View>
-            <Caption text="CONNECT" />
+            <Caption text="CONNECT" style={styles.title}/>
           </View>
         </View>
         <View>
           <Caption
-            text="Welcome to our Connect community"
+            text="Welcome to our Connect community" style={styles.subtitle}
           />
         </View>
       </View>
       <View>
-      <Caption text="Login to Connect"  />
+      <Caption text="Login to Connect" style={styles.connect} />
       </View>
     </>
   );
