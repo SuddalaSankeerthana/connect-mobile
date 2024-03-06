418daf75e913d08a39ca342adaddcf8fa5328ae5
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2024-03-06 12:41:51 +0530

    Implement navigation

--- ConnectAppMobile/App.tsx
+++ ConnectAppMobile/App.tsx
@@ -1,62 +1,46 @@
-// import React from 'react';
-// import {NavigationContainer} from '@react-navigation/native';
-// import {createNativeStackNavigator} from '@react-navigation/native-stack';
-// // import HomeScreen from './screens/Home';
-// // import LoginScreen from './screens/Login';
-// // import RegistrationScreen from './screens/Registration';
-// import UploadScreen from './screens/Upload';
-
-// type AppStackParamList = {
-//   Home: undefined;
-//   Upload: undefined;
-//   Login: undefined;
-//   Registration: undefined;
-// };
-
-// const Stack = createNativeStackNavigator<AppStackParamList>();
-
-// function AppStack() {
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         {/* <Stack.Screen
-//           name="Login"
-//           component={LoginScreen}
-//           options={{headerShown: false}}
-//         /> */}
-//         {/* <Stack.Screen
-//           name="Registration"
-//           component={RegistrationScreen}
-//           options={{headerShown: false}}
-//         /> */}
-//         <Stack.Screen
-//           name="Upload"
-//           component={UploadScreen}
-//           options={{headerShown: false}}
-//         />
-//         {/* <Stack.Screen
-//           name="Home"
-//           component={HomeScreen}
-//           options={{headerShown: false}}
-//         /> */}
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-// export default AppStack;
-
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import UploadScreen  from './screens/Upload';
-import { styles } from './styles/UploadPostScreen.styles';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from './src/screens/Home';
+import LoginScreen from './src/screens/Login';
+import RegistrationScreen from './src/screens/Registration';
+import UploadScreen from './src/screens/Upload';
 
+type AppStackParamList = {
+  Home: undefined;
+  Upload: undefined;
+  Login: undefined;
+  Registration: undefined;
+};
 
-export default function App() {
+const Stack = createNativeStackNavigator<AppStackParamList>();
+
+function AppStack() {
   return (
-    <SafeAreaView
-      style={styles.SafeAreaView}
-    >
-      <UploadScreen />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        /> 
+        <Stack.Screen
+          name="Registration"
+          component={RegistrationScreen}
+          options={{headerShown: false}}
+        />
+        <Stack.Screen
+          name="Upload"
+          component={UploadScreen}
+          options={{headerShown: false}}
+        />
+         <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+export default AppStack;
