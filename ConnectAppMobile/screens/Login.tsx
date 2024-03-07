07f8b9ae40b490f37aa5afe0fea17d786c82d263
PagidipallyDivyasai <divya.pagidipally@everest.engineering>
2024-03-07 12:10:36 +0530

    Add login handle and validations handle at login button

--- ConnectAppMobile/screens/Login.tsx
+++ ConnectAppMobile/screens/Login.tsx
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { SafeAreaView, View} from 'react-native';
 import LoginLogo from '../src/components/Logo';
 import InputField from '../src/components/InputFields';
 import LoginButton from '../src/components/Button';
 import Caption from '../src/components/Caption';
 import {styles} from '../src/styles/Login.styles';
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { useNavigation } from '@react-navigation/native';
 
 function LoginScreen(): React.JSX.Element {
-
   const [email, setEmail]= React.useState('');
   const [password, setPassword]= React.useState('');
+  const userData={
+    email: email,
+    password: password
+  }
   return (
     <SafeAreaView>
       <View style={styles.mainContainer}>
@@ -26,7 +27,7 @@ function LoginScreen(): React.JSX.Element {
             source={require('../src/assets/password.png')}
             style={styles.passwordIcon}
           />
-          <LoginButton password={password} email={email} />
+          <LoginButton password={password} email={email} userData={userData} />
           <View style={styles.noAccount}>
           <Caption text="Don't have an account?" />
           <Caption text="Sign Up" style={styles.signUp} />
