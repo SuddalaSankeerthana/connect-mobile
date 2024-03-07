c13cc239e98ca7202f55f5c9d609ea27d5ac7766
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-07 14:32:49 +0530

    fetch posts data from backend.

--- ConnectAppMobile/src/components/Body.tsx
+++ ConnectAppMobile/src/components/Body.tsx
@@ -1,24 +1,44 @@
-import { FlatList} from 'react-native';
-import {PostDetails, PostsData} from '../seeder/PostDetails';
-import { PostItem } from '../components/PostItem';
-import React from 'react';
+import {FlatList} from 'react-native';
+import {PostItem} from '../components/PostItem';
+import React, {useEffect} from 'react';
+import {useState} from 'react';
 
-const renderPostDetails = ({item}: {item: PostDetails}) => {
+export type PostType = {
+  PostId: string;
+  UserId?: string;
+  Username: string;
+  ProfileImageAddress: string;
+  Images: [];
+  Caption: string;
+  LikesCount: number;
+};
+const renderPostDetails = ({item}: {item: PostType}) => {
   return (
     <PostItem
-      id={item.id}
-      profile_pic={item.profile_pic}
-      name={item.name}
-      images={item.images}
-      description={item.description}
-      likes_count={item.likes_count}
+      PostId={item.PostId}
+      ProfileImageAddress={item.ProfileImageAddress}
+      Username={item.Username}
+      Images={item.Images}
+      Caption={item.Caption}
+      LikesCount={item.LikesCount}
     />
   );
 };
 export function Body() {
-  return (
-    <>
-        <FlatList data={PostsData} renderItem={renderPostDetails} />
-    </>
-  );
+  const [data, setData] = useState<PostType[]>([]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        'http://localhost:8080/homepage/get-posts',
+      ).then(res => {
+        return res.json();
+      });
+      const posts: PostType[] = Object.values(response.posts);
+      setData(posts);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+  return <FlatList data={data} renderItem={renderPostDetails} />;
 }
