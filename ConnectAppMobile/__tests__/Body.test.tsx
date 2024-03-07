f76118073c2a75a1cd51ba383ece3359cc1fd13c
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-08 01:40:31 +0530

    Test: add tests for calling the fetch function everytime when the body renders

--- ConnectAppMobile/__tests__/Body.test.tsx
+++ ConnectAppMobile/__tests__/Body.test.tsx
@@ -1,14 +1,31 @@
 import 'react-native';
+import { ReactTestRenderer } from 'react-test-renderer';
 import React from 'react';
-import {render, screen} from '@testing-library/react-native';
-import { Body } from '../src/components/Body';
+import {
+  act,
+} from '@testing-library/react-native';
+import TestRenderer from 'react-test-renderer';
+import {Body} from '../src/components/Body';
+import { fetchData } from '../src/utils/fetchData';
 
+jest.mock('../src/utils/fetchData')
 jest.mock('@fortawesome/react-native-fontawesome', () => ({
-    FontAwesomeIcon: ''
-}))
+  FontAwesomeIcon: '',
+}));
 
-test('renders correctly', () => {
-  render(
-    <Body />
-  );
-});
+describe("test rendering",()=>
+{
+  
+  test("render posts",()=>
+  {
+    
+    let renderer: ReactTestRenderer;
+  act(() => {
+    renderer = TestRenderer.create(<Body />);
+  });
+  
+    expect(fetchData).toHaveBeenCalledTimes(1);
+    
+    
+  })
+})
