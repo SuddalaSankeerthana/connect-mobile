649ca52969c6cf335670ae74f25ddc0ace3cc0a5
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-08 01:52:31 +0530

    remove unused configuring file.

--- ConnectAppMobile/setUpJest.js
+++
@@ -1,5 +0,0 @@
-require('jest-fetch-mock').enableMocks()
-global.fetch = require('jest-fetch-mock');
-import '@testing-library/jest-dom';
-import fetchMock from "jest-fetch-mock";
-fetchMock.enableMocks();
