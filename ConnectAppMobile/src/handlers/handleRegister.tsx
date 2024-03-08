df5e5db87bbc9639303d7831172de3ac9007c6e8
PagidipallyDivyasai <divya.pagidipally@everest.engineering>
2024-03-08 09:42:30 +0530

    Handle registration onclicking registration button

--- ConnectAppMobile/src/handlers/handleRegister.tsx
+++ ConnectAppMobile/src/handlers/handleRegister.tsx
@@ -1,30 +1,32 @@
+import { Alert, Platform } from "react-native";
 import { RegisterRouteProps } from "../types/RegisterRouteProps"
 export const handleRegister=async (userData:RegisterRouteProps, navigation: any)=>{
-  console.log(userData);
-  try{
-    const response= await fetch("http://10.0.2.2:8080/auth/register",{
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json' 
-    },
-    body: JSON.stringify(userData)
-  }).then((res)=>{
-    return res
-  })
-  if (response.status === 201) {
-    navigation.navigate('Home')
-    alert("User registered successfully!");
+  try {
+    const url =
+      Platform.OS == 'android'
+        ? 'http://10.0.2.2:8080/auth/register'
+        : 'http://localhost:8080/auth/register';
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    }).then(res => {return res;});
+    if (response.status === 201) {
+      navigation.navigate('Home')
+      alert("User registered successfully!");
+    }
+    else if (response.status === 401) {
+      alert("User already existed!");
+    }
+    else {
+      alert("Something went wrong");
+    }
   }
-  else if (response.status === 401) {
-    alert("User already existed!");
+  catch (error) {
+    console.error("Error registering user:", error);
+    alert("Failed to register user. Please try again later.");
   }
-  else {
-    alert("Something went wrong");
-  }
-}
-catch (error) {
-  console.error("Error registering user:", error);
-  alert("Failed to register user. Please try again later.");
-}
-return true
+  return true
 }
