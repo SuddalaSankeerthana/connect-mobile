9ab45abf6d7a213d8d8ffd552b5bd876d5a4c6ce
PagidipallyDivyasai <divya.pagidipally@everest.engineering>
2024-03-08 10:00:04 +0530

    Add all test cases for registration validation

--- ConnectAppMobile/__tests__/Registration.test.tsx
+++ ConnectAppMobile/__tests__/Registration.test.tsx
@@ -16,6 +16,7 @@ import SigIn from '../src/components/registrationScreen/SignIn';
 import SigInContent from '../src/components/registrationScreen/SiginContent';
 import RegistrationScreen from '../src/screens/Registration';
 import { Alert } from 'react-native';
+import { handleRegister } from '../src/handlers/handleRegister';
 
 describe('Test for registration screen individual components', () => {
   test('test for background image component', () => {
@@ -273,6 +274,156 @@ describe("Test for input validations in registraion screen",()=>{
     const register= screen.getByText('Register')
     expect(register).toBeDefined();
     fireEvent.press(register)
-    // expect(Alert.alert).toHaveBeenCalledWith("");
+    expect(Alert.alert).toHaveBeenCalled();
+
   })
+
+  test("To check for missmatch password",()=>{
+    const navigation = {
+      navigate: jest.fn(() => {}),
+    };
+    const userDataValidations={
+      username: "harika",
+      email: "hari@gmail.com",
+      password: "HarI@313#",
+      confirmPassword: "HarI",
+     }
+    render(<RegisterButton navigation={navigation} userData={userDataValidations}></RegisterButton>);
+    jest.spyOn(Alert, 'alert');
+    const register= screen.getByText('Register')
+    expect(register).toBeDefined();
+    fireEvent.press(register)
+    expect(Alert.alert).toHaveBeenCalled();
+
+
+  })
+
+  test("To check for not including all the characters",()=>{
+    const navigation = {
+      navigate: jest.fn(() => {}),
+    };
+    const userDataValidations={
+      username: "harika",
+      email: "hari@gmail.com",
+      password: "Hari3145",
+      confirmPassword: "Hari3145",
+     }
+    render(<RegisterButton navigation={navigation} userData={userDataValidations}></RegisterButton>);
+    jest.spyOn(Alert, 'alert');
+    const register= screen.getByText('Register')
+    expect(register).toBeDefined();
+    fireEvent.press(register)
+    expect(Alert.alert).toHaveBeenCalled();
+
+  })
+
+  test("To check for not including eight characters",()=>{
+    const navigation = {
+      navigate: jest.fn(() => {}),
+    };
+    const userDataValidations={
+      username: "harika",
+      email: "hari@gmail.com",
+      password: "Hari314",
+      confirmPassword: "Hari314",
+     }
+    render(<RegisterButton navigation={navigation} userData={userDataValidations}></RegisterButton>);
+    jest.spyOn(Alert, 'alert');
+    const register= screen.getByText('Register')
+    expect(register).toBeDefined();
+    fireEvent.press(register)
+    expect(Alert.alert).toHaveBeenCalled();
+
+  })
+
+  test("To check for not including uppercase characters",()=>{
+    const navigation = {
+      navigate: jest.fn(() => {}),
+    };
+    const userDataValidations={
+      username: "harika",
+      email: "hari@gmail.com",
+      password: "hari314",
+      confirmPassword: "hari314",
+     }
+    render(<RegisterButton navigation={navigation} userData={userDataValidations}></RegisterButton>);
+    jest.spyOn(Alert, 'alert');
+    const register= screen.getByText('Register')
+    expect(register).toBeDefined();
+    fireEvent.press(register)
+    expect(Alert.alert).toHaveBeenCalled();
+  })
+
+
+  test("To check for not including lowercase characters",()=>{
+    const navigation = {
+      navigate: jest.fn(() => {}),
+    };
+    const userDataValidations={
+      username: "harika",
+      email: "hari@gmail.com",
+      password: "HARI314",
+      confirmPassword: "HARI314",
+     }
+    render(<RegisterButton navigation={navigation} userData={userDataValidations}></RegisterButton>);
+    jest.spyOn(Alert, 'alert');
+    const register= screen.getByText('Register')
+    expect(register).toBeDefined();
+    fireEvent.press(register)
+    expect(Alert.alert).toHaveBeenCalled();
+
+  })
+
+  test("To check for not including digits ",()=>{
+    const navigation = {
+      navigate: jest.fn(() => {}),
+    };
+    const userDataValidations={
+      username: "harika",
+      email: "hari@gmail.com",
+      password: "Harikasjd",
+      confirmPassword: "Harikasjd",
+     }
+    render(<RegisterButton navigation={navigation} userData={userDataValidations}></RegisterButton>);
+    jest.spyOn(Alert, 'alert');
+    const register= screen.getByText('Register')
+    expect(register).toBeDefined();
+    fireEvent.press(register)
+    expect(Alert.alert).toHaveBeenCalled();
+  })
+
+  test("To check if all validations are correct ",()=>{
+    const navigation = {
+      navigate: jest.fn(() => {}),
+    };
+    const userDataValidations={
+      username: "harika",
+      email: "hari@gmail.com",
+      password: "Harika@123#",
+      confirmPassword: "Harika@123#",
+     }
+    render(<RegisterButton navigation={navigation} userData={userDataValidations}></RegisterButton>);
+    const register= screen.getByText('Register')
+    expect(register).toBeDefined();
+    fireEvent.press(register)
+    expect(Alert.alert).toHaveBeenCalled();
+  })
+
+  test("To check if all validations are correct ",()=>{
+    const navigation = {
+      navigate: jest.fn(() => {}),
+    };
+    const userDataValidations={
+      username: "harika",
+      email: "hari@geverest.engineering",
+      password: "Harika@123#",
+      confirmPassword: "Harika@123#",
+     }
+    render(<RegisterButton navigation={navigation} userData={userDataValidations}></RegisterButton>);
+    const register= screen.getByText('Register')
+    expect(register).toBeDefined();
+    fireEvent.press(register)
+    expect(Alert.alert).toHaveBeenCalled();
+  })
+
   })
