43fa8ca9ac815180f04123fb31955ad40ec1acbb
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2024-03-08 16:50:40 +0530

    Add states to handle the caption input

--- ConnectAppMobile/src/screens/Upload.tsx
+++ ConnectAppMobile/src/screens/Upload.tsx
@@ -1,30 +1,27 @@
 import React, {useState} from 'react';
-import {
-  Image,
-    SafeAreaView,
-    Text,
-  TextInput,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import {styles} from '../styles/UploadPostScreen.styles';
+import { Image, SafeAreaView, Text, TouchableOpacity, View } from 'react-native';
 import ImagePicker, { ImageOrVideo } from 'react-native-image-crop-picker';
+import RNFS from 'react-native-fs';
+import {styles} from '../styles/UploadPostScreen.styles';
 import { CancelButton } from '../components/CancelButton';
 import UploadIcon from '../components/UploadIcon';
 import { UploadButton } from '../components/UploadButton';
 import { TextInputContainer } from '../components/TextInputContainer';
 
 
-const UploadScreen = ({navigation}: any) => {
+
+const UploadScreen = ({ navigation }: any) => {
   const [selectedImages, setSelectedImages] = useState<ImageOrVideo[]>([]);
   const [imagesSelected, setImagesSelected] = useState(false);
   const [warningMessage, setWarningMessage] = useState<string>('');
+  const [caption, setCaption] = useState<string>('');
 
   const handleUploadIconPress = async () => {
     const images = await ImagePicker.openPicker({
       multiple: true,
       mediaType: 'photo',
     });
+
     if (images.length <= 4) {
       setWarningMessage('');
       setSelectedImages(images);
@@ -35,6 +32,8 @@ const UploadScreen = ({navigation}: any) => {
       setWarningMessage('You can select only upto four images');
     }
   };
+
+
   const handleCancel = () => {};
   const handleUpload = () => {};
   return (
@@ -87,7 +86,12 @@ const UploadScreen = ({navigation}: any) => {
       </View>
       {imagesSelected && (
         <>
-          <TextInputContainer placeholder="Add a caption" placeholderTextColor="#A0A0A0"/>
+          <TextInputContainer
+            placeholder="Add a caption"
+            placeholderTextColor="#A0A0A0"
+            onChangeText={setCaption}
+            value={caption}
+          />
           <View style={styles.buttonContainer}>
             <TouchableOpacity onPress={handleCancel}>
               <CancelButton testID="cancel-button" text="Cancel" />
