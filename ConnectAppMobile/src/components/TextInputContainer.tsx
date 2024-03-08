a56a64081df6fb5fbb5c3d24887658a7ea078b84
Meghana Gaddam <meghana.gaddam@everest.engineering.com>
2024-03-08 16:51:14 +0530

    Update the props

--- ConnectAppMobile/src/components/TextInputContainer.tsx
+++ ConnectAppMobile/src/components/TextInputContainer.tsx
@@ -5,10 +5,12 @@ import React from 'react';
 type TextInputProps = {
   placeholder: string;
   placeholderTextColor: string;
+  onChangeText: (text: string) => void;
+  value: string
 };
-export const TextInputContainer = ({ placeholder, placeholderTextColor }: TextInputProps) => (
+export const TextInputContainer = ({ placeholder, placeholderTextColor,onChangeText,value }: TextInputProps) => (
   <View style={styles.textInputContainer}>
-    <TextInput placeholder={placeholder} placeholderTextColor={placeholderTextColor} />
+    <TextInput placeholder={placeholder} placeholderTextColor={placeholderTextColor} onChangeText={onChangeText} value={value} />
   </View>
 );
 
