d4654538f37e007edbb2c6c100bf6abc942a86f0
SuddalaSankeerthana <suddalakeerthana@gmail.com>
2024-03-09 19:50:46 +0530

    Update current user context value with provider in homeScreen

--- ConnectAppMobile/src/screens/Home.tsx
+++ ConnectAppMobile/src/screens/Home.tsx
@@ -4,16 +4,17 @@ import {SafeAreaView} from 'react-native';
 import {Body} from '../components/homeScreen/Body';
 import {Header} from '../components/homeScreen/Header';
 import {styles} from '../styles/HomeScreenBodyStyles';
-
+import {CurrentUserContext} from '../components/homeScreen/LikeContext';
 
 export const HomeScreen = ({navigation}: any) => {
   return (
     <SafeAreaView>
-      <View style={styles.homeContainer}>
-        <Header navigation={navigation} />
-        <Body />
-      </View>
+      <CurrentUserContext.Provider value={{UserId: '3'}}>
+        <View style={styles.homeContainer}>
+          <Header navigation={navigation} />
+          <Body />
+        </View>
+      </CurrentUserContext.Provider>
     </SafeAreaView>
   );
 };
-
