72b05640719f655d5958c8995601678962b1280d
SuddalaSankeerthana <suddalakeerthana@gmail.com>
2024-03-09 21:27:19 +0530

    Update like context

--- ConnectAppMobile/src/components/homeScreen/LikeContext.tsx
+++ ConnectAppMobile/src/components/homeScreen/LikeContext.tsx
@@ -12,5 +12,19 @@ export type User = {
   UserId: string;
 };
 export const CurrentUserContext = React.createContext<User>({UserId: '3'});
-export const LikeContext = React.createContext<LikeContextType>();
-export const PostContext = React.createContext<PostType>();
+export const LikeContext = React.createContext<LikeContextType>({
+  likesCount: 0,
+  likeStatus: false,
+  setLikesCount: () => {},
+  setLikeStatus: () => {},
+});
+export const PostContext = React.createContext<PostType>({
+  PostId: '',
+  UserId: '',
+  Username: '',
+  ProfileImageAddress: '',
+  Images: [],
+  Caption: '',
+  LikesCount: 0,
+  LikeStatus: false,
+});
