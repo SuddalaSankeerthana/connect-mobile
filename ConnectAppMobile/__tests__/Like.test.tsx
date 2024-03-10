13ec41d445d188127da499fb9222d2524e36f110
Dhanalaxmi-Dikonda <dhanalaxmidikonda180@gmail.com>
2024-03-11 04:03:43 +0530

    add like tests

--- ConnectAppMobile/__tests__/Like.test.tsx
+++ ConnectAppMobile/__tests__/Like.test.tsx
@@ -10,8 +10,7 @@ import {act, waitFor} from '@testing-library/react-native';
 import TestRenderer from 'react-test-renderer';
 import {Body} from '../src/components/homeScreen/Body';
 import {fetchData} from '../src/utils/fetchData';
-import { NavigationContainer } from '@react-navigation/native';
-
+import {NavigationContainer} from '@react-navigation/native';
 
 jest.mock('@fortawesome/react-native-fontawesome', () => ({
   FontAwesomeIcon: '',
@@ -21,14 +20,18 @@ jest.mock('../src/utils/fetchData');
 describe('Test suit for liked Button component', () => {
   beforeEach(() => {
     global.fetch = jest.fn();
-    console.error=jest.fn();
+    console.error = jest.fn();
   });
   afterAll(() => console.log('1 - afterAll'));
 
   it('render posts', async () => {
     let renderer: ReactTestRenderer;
     act(() => {
-      renderer = TestRenderer.create(<NavigationContainer><Body/></NavigationContainer>);
+      renderer = TestRenderer.create(
+        <NavigationContainer>
+          <Body />
+        </NavigationContainer>,
+      );
     });
     await waitFor(() => {
       expect(fetchData).toHaveBeenCalledTimes(1);
